a865003bfc1c666aa72fbdf7886232f9b45819fa
EmKry <krysztofiak.marcin@gmail.com>
2019-11-20 00:45:12 +0100

    zmiana 2

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 wzrost=1.8
 waga=96
 BMI=waga/(wzrost**2)
-print("\n\nTwoje BMI wynosi:",BMI,"(\n\n")
+wiek=31
+akt=1.4
+s=5
+kcal=10*waga+6.25*wzrost*100-5*wiek+s
+print("\n\nTwoje BMI wynosi:",BMI,"(\n")
+print("Twoje zapotrzebowanie kaloryczne wynosi:",kcal,"kcal\n")
+
